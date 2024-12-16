40371199d28412dba0771d0d31ff107e8607639d
Keith Noguchi <keith@noguchi.us>
2024-12-15 21:33:07 -0800

    main.go: Implement priority queues
    It's one for the priority and the other for the expiry
    queue.

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 package main
 
 import (
-	_ "container/heap"
+	"container/heap"
 	"time"
 )
 
@@ -10,15 +10,15 @@ import (
 type Cache struct {
 	maxItems  int
 	table     map[string]*Item
-	expiryQ   expiryQueue
-	priorityQ priorityQueue
+	priorityQ PriorityQueue
+	expiryQ   ExpiryQueue
 }
 
 func NewCache(maxItems int) *Cache {
-	var expiryQ expiryQueue
-	var priorityQ priorityQueue
-	//heap.Init(&expiryQ)
-	//heap.Init(&priorityQ)
+	var priorityQ PriorityQueue
+	heap.Init(&priorityQ)
+	var expiryQ ExpiryQueue
+	heap.Init(&expiryQ)
 	return &Cache{
 		maxItems:  maxItems,
 		table:     make(map[string]*Item),
@@ -68,8 +68,39 @@ func (c *Cache) SetMaxItems(maxItems int) {
 func (c *Cache) evictItems() {
 }
 
-type expiryQueue []*Item
-type priorityQueue []*Item
+// https://pkg.go.dev/container/heap#example-package-PriorityQueue
+type BaseQueue []*Item
+type ExpiryQueue struct {
+	BaseQueue
+}
+type PriorityQueue struct {
+	BaseQueue
+}
+
+func (pq BaseQueue) Len() int { return len(pq) }
+func (pq BaseQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+func (pq *BaseQueue) Push(x any) {
+	item := x.(*Item)
+	*pq = append(*pq, item)
+}
+func (pq *BaseQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	*pq = old[0 : n-1]
+	return item
+}
+func (pq ExpiryQueue) Less(i, j int) bool {
+	// To pop the oldest expire time item first.
+	return pq.BaseQueue[i].expire.Before(pq.BaseQueue[j].expire)
+}
+func (pq PriorityQueue) Less(i, j int) bool {
+	// To pop the lowest priority item first.
+	return pq.BaseQueue[i].priority < pq.BaseQueue[j].priority
+}
 
 func main() {
 	c := NewCache(5)
